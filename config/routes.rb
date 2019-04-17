689adca05fef3416b0bce296f453569f6b79ac09
Brooke Ma <65210238@qq.com>
2019-04-17 22:31:45 +0800

    did half of it

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/new'
-  get 'restaurants/create'
-  get 'restaurants/show'
+  root to: 'restaurants#index'
+  # get 'reviews/new', to: 'restaurants#index'
+  # get 'reviews/create', to: 'restaurants#index'
+  # get 'restaurants/index'
+  # get 'restaurants/new'
+  # get 'restaurants/create'
+  # get 'restaurants/show'
+  resource :restaurants do
+    resource :review, shallow: true
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
